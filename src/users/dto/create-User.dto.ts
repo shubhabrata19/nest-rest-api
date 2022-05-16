d750700694c015da577d718d7f04e31bccdb8f81
Shubhabrata Ghosh <shubhabratarocks@gmail.com>
2022-05-16 16:22:07 +0530

    Param error in Swagger resolved

--- src/users/dto/create-User.dto.ts
+++ src/users/dto/create-User.dto.ts
@@ -1,6 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    type: String,
+    description: 'The id of the user',
+    default: '',
+  })
   readonly id: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'The Name of the user',
+    default: '',
+  })
   readonly name: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'The email of the user',
+    default: '',
+  })
   readonly email: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'The Password of the user',
+    default: '',
+  })
   readonly password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,32 +10,39 @@ import {
 import { CreateUserDto } from './dto/create-User.dto';
 import { UsersService } from './users.service';
 import { User } from './interfaces/user.interface';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users APIs')
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get()
+  @ApiOkResponse({ description: 'Users fetched Sucessfully' })
   getAllUser(): Promise<User[]> {
     return this.userService.findAll();
   }
 
   @Get(':id')
-  getOneUser(@Param() param): Promise<User> {
-    return this.userService.findOne(param.id);
+  @ApiOkResponse({ description: 'User fetched Sucessfully' })
+  getOneUser(@Param('id') id: string): Promise<User> {
+    return this.userService.findOne(id);
   }
 
   @Post()
+  @ApiOkResponse({ description: 'User created Sucessfully' })
   createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.create(createUserDto);
   }
 
   @Delete(':id')
-  deleteUser(@Param('id') id): Promise<User> {
+  @ApiOkResponse({ description: 'Users deleted Sucessfully' })
+  deleteUser(@Param('id') id: string): Promise<User> {
     return this.userService.delete(id);
   }
 
   @Put(':id')
+  @ApiOkResponse({ description: 'Users updated Sucessfully' })
   updateUser(
     @Body() updateUserDto: CreateUserDto,
     @Param('id') id,
